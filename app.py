a9d12a4610c4d869318fd1f9fc6234017f6bcd72
Prajwal raj <47392217+Prajwal270@users.noreply.github.com>
2021-08-30 12:54:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+
+from flask import Flask
+from flask import render_template, url_for
+app = Flask(__name__)
+
+@app.route('/harry')
+def harry():
+    return "hello harry"
+@app.route("/new")
+def new():
+    name = "Prajwal"
+    return render_template('index.html', name = name )
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
